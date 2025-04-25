994d60bd95d3362ea7705de3008a0fb2b35ce7dd
jhay20x <jhay20x@gmail.com>
2025-04-25 13:40:50 +0800

    Update 4.25

--- users/secretary/php/update-dashboard-data.php
+++ users/secretary/php/update-dashboard-data.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_username']) && isset($_
     $res = $conn->query("SELECT CONNECTION_ID()");
     $row = $res->fetch_row();
     header("X-DB-Conn-ID: " . $row[0]);
-    $stmt->close();
+    $res->close();
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
