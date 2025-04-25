ff969a6c9868f3fc99f853d28d900aaadfe8df60
jhay20x <jhay20x@gmail.com>
2025-04-25 13:58:43 +0800

    Update 4.25

--- users/secretary/php/fetch-requests.php
+++ users/secretary/php/fetch-requests.php
@@ -18,6 +18,11 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_username']) && isset($_
     $result = $stmt->get_result();
 	$stmt->close();
 
+    $res = $conn->query("SELECT CONNECTION_ID()");
+    $row = $res->fetch_row();
+    header("X-DB-Conn-ID: " . $row[0]);
+    $res->close();
+
     if ($result->num_rows > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
             $data[] = [
